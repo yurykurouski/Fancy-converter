c134e2cf44b942c017b0de9b0073b7bf2a71e83d
Yury Kurouski <ymorpheus@gmail.com>
2021-11-20 15:15:50 +0300

    feat: make currencies inputs area scrollable

--- src/components/CurrencySelector/CurrencySelector.tsx
+++ src/components/CurrencySelector/CurrencySelector.tsx
@@ -1,10 +1,11 @@
 import React, { useContext, useState } from 'react';
-import { Button } from 'react-native';
+import { Button, ScrollView } from 'react-native';
 
 import { FocusedCurrencyProvider } from '../Context/FocusedCurrencyContext';
 import { SelectedCurrenciesContext } from '../Context/SelectedCurrenciesContext';
 import { CurrenciesModal } from '../CurrenciesModal/CurrenciesModal';
 import { CurrencyValue } from '../CurrencyValue';
+import { styles } from './CurrencySelector.styles';
 
 export const CurrencySelector = ({ exchangeCourse }) => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -18,16 +19,17 @@ export const CurrencySelector = ({ exchangeCourse }) => {
         setModalVisible={setModalVisible}
         modalVisible={modalVisible}
       />
-
-      {selectedCurrencies.length > 0 && (
-        <FocusedCurrencyProvider>
-          <CurrencyValue
-            selectedCurrencies={selectedCurrencies}
-            exchangeCourse={exchangeCourse}
-          />
-        </FocusedCurrencyProvider>
-      )}
-      <Button onPress={() => setModalVisible(true)} title="Add a currency" />
+      <ScrollView style={styles.container}>
+        {selectedCurrencies.length > 0 && (
+          <FocusedCurrencyProvider>
+            <CurrencyValue
+              selectedCurrencies={selectedCurrencies}
+              exchangeCourse={exchangeCourse}
+            />
+          </FocusedCurrencyProvider>
+        )}
+        <Button onPress={() => setModalVisible(true)} title="Add a currency" />
+      </ScrollView>
     </>
   );
 };
