42e13270f9fd424699c3cdb19f3b2501309dd38e
Yury Kurouski <ymorpheus@gmail.com>
2022-01-09 19:58:56 +0300

    feat: cache courses in storage

--- src/hooks/useGetCurrenciesExchangeCourse.tsx
+++ src/hooks/useGetCurrenciesExchangeCourse.tsx
@@ -1,5 +1,10 @@
 import { useCallback, useEffect, useState } from 'react';
-import { getOnlyCourses } from 'utils';
+import {
+  getFromStorage,
+  getOnlyCourses,
+  setToStorage,
+  StorageKeys,
+} from 'utils';
 import { OnlyCourses } from 'utils/utils.types';
 
 import { currenciesService } from '../services/currencies-service';
@@ -15,6 +20,40 @@ export const useGetCurrenciesExchangeCourse: UseGetCurrenciesExchangeCourse =
     const [isLoading, setIsLoading] = useState(false);
     const [exchangeCourse, setExchangeCourse] = useState(null);
 
+    const isCoursesCheckedToday = useCallback(async () => {
+      const currentDate = new Date();
+      const lastCoursesUpdate = await getFromStorage(
+        StorageKeys.LAST_COURSES_UPDATE,
+      );
+
+      if (!lastCoursesUpdate) {
+        await setToStorage(
+          StorageKeys.LAST_COURSES_UPDATE,
+          currentDate.getTime(),
+        );
+
+        return false;
+      }
+
+      const lastUpdateDate = new Date(Number(lastCoursesUpdate));
+      return (
+        currentDate.getDate() == lastUpdateDate.getDate() &&
+        currentDate.getMonth() == lastUpdateDate.getMonth() &&
+        currentDate.getFullYear() == lastUpdateDate.getFullYear()
+      );
+    }, []);
+
+    const getCoursesFromStorage = useCallback(() => {
+      setIsLoading(true);
+
+      getFromStorage(StorageKeys.EXCHANGE_COURSES)
+        .then(value => {
+          const parsed = JSON.parse(value);
+          setExchangeCourse(parsed);
+        })
+        .then(() => setIsLoading(false));
+    }, []);
+
     const reloadCourses = useCallback(() => {
       setIsLoading(true);
 
@@ -23,13 +62,20 @@ export const useGetCurrenciesExchangeCourse: UseGetCurrenciesExchangeCourse =
         .then(value => {
           const onlyCourses = getOnlyCourses(value);
           setExchangeCourse(onlyCourses);
+          setToStorage(StorageKeys.EXCHANGE_COURSES, onlyCourses);
         })
         .then(() => setIsLoading(false));
     }, []);
 
     useEffect(() => {
-      reloadCourses();
-    }, [reloadCourses]);
+      isCoursesCheckedToday().then(isCheckedToday => {
+        if (!isCheckedToday) {
+          reloadCourses();
+        } else {
+          getCoursesFromStorage();
+        }
+      });
+    }, [getCoursesFromStorage, isCoursesCheckedToday, reloadCourses]);
 
     return { isLoading, exchangeCourse, reloadCourses };
   };
