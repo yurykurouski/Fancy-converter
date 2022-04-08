c13fec717f887e971d17b1d88a3cb6ce865780c5
Yury Kurouski <ymorpheus@gmail.com>
2022-04-08 18:57:29 +0400

    feat: complete UA localization

--- src/resources/localization.ts
+++ src/resources/localization.ts
@@ -2,8 +2,12 @@ import LocalizedStrings from 'react-native-localization';
 
 import by from './by.json';
 import en from './en.json';
+import ua from './ua.json';
 
 export const l = new LocalizedStrings({
   en,
   'be-BY': by,
+  'uk-UA': ua,
 });
+
+console.log(l.getInterfaceLanguage());
