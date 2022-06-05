7c42e850323875636eea55019db264c9c0bfdf3e
Yury Kurouski <ymorpheus@gmail.com>
2022-06-05 13:41:05 +0400

    feat: change naming

---
+++ src/types/index.ts
@@ -0,0 +1,43 @@
+export enum AvaliableCurrenciesNames {
+  USD = 'USD',
+  EUR = 'EUR',
+  RUB = 'RUB',
+  GBP = 'GBP',
+  CAD = 'CAD',
+  PLN = 'PLN',
+  UAH = 'UAH',
+  SEK = 'SEK',
+  CHF = 'CHF',
+  JPY = 'JPY',
+  CNY = 'CNY',
+  CZK = 'CZK',
+  NOK = 'NOK',
+  BYN = 'BYN',
+  GEL = 'GEL',
+}
+
+export type CurrencyDescription = {
+  Cur_ID?: number;
+  Date?: string;
+  Cur_Abbreviation: string;
+  Cur_Scale?: number;
+  Cur_Name: string;
+  Cur_OfficialRate?: number;
+  Cur_Symbol?: string;
+};
+
+export type AvaliableCurrenciesInObject = {
+  [x: string]: CurrencyDescription;
+};
+
+export type AvaliableCurrenciesInArray = {
+  [key in AvaliableCurrenciesNames]?: CurrencyDescription;
+};
+
+export type ResultFromAPI = CurrencyDescription[];
+
+export type CurrenciesCourses = {
+  [key in AvaliableCurrenciesNames]?: CurrencyDescription;
+};
+
+export type SelectedCurrencies = string[] | [];
