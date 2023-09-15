f204558c3fc645da093a87a47dfbca1f66942c6f
Yury Kurouski <yury_kurouski@epam.com>
2023-09-15 22:03:50 +0200

    fix on ios

--- ios/Converter/AppDelegate.mm
+++ ios/Converter/AppDelegate.mm
@@ -60,7 +60,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
 
-  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView];
+  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:self.window.rootViewController.view];
   return YES;
 }
 
