a60752c84417f3c0021834650ed47025a4c9703a
Yury Kurouski <yury_kurouski@epam.com>
2023-09-15 23:29:57 +0200

    feat: add platform utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './compareDate';
 export * from './getCurrentColorTheme';
 export * from './getOnlyCourses';
+export * from './platform';
 export * from './showNoConnectionAlert';
 export * from './storage';
