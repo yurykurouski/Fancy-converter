7d1614fec79da14817e2eb2399c74dd25b622cfe
Yury Kurouski <yury_kurouski@epam.com>
2023-09-16 13:09:09 +0200

    fix: change notification animation on dynamic island

--- src/context/MessageNotificationContext/Notification.tsx
+++ src/context/MessageNotificationContext/Notification.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import { Animated, Text } from 'react-native';
+import { useAppState } from '@react-native-community/hooks';
+import { useIsHasIsland } from 'hooks/useHasIsland';
 
-import { animatedPosition } from './notification-animations';
+import { animatedPosition, animatedWidth } from './notification-animations';
 
 import { useStyles } from './WithNotification.styles';
 
+export const TOP_INSET_WITH_ISLAND = 59;
+
 const Notification = ({ message }: { message: string }) => {
   const styles = useStyles();
+
+  const hasIsland = useIsHasIsland();
+
+  const widthInterpolate = animatedWidth.interpolate({
+    inputRange: [0, 1],
+    outputRange: [124, 200],
+  });
+
+  const appState = useAppState();
+
+  const opacity = appState === 'inactive' ? 0 : 1;
+
+  if (hasIsland) {
+    return (
+      <Animated.View
+        style={[
+          styles.container,
+          styles.withIslandContainer,
+          {
+            transform: [{ translateY: animatedPosition }],
+          },
+          { width: widthInterpolate, opacity },
+        ]}>
+        <Text style={styles.text}>{message}</Text>
+      </Animated.View>
+    );
+  }
+
   return (
     <Animated.View
       style={[
--- src/context/MessageNotificationContext/notification-animations.ts
+++ src/context/MessageNotificationContext/notification-animations.ts
@@ -28,3 +28,52 @@ export const showNotification = () =>
       easing: Easing.exp,
     }),
   ]).start();
+
+export const animatedWidth = new Animated.Value(0);
+export const showNotificationForIsland = () =>
+  Animated.sequence([
+    Animated.delay(500),
+    Animated.parallel([
+      Animated.timing(animatedPosition, {
+        toValue: 44,
+        duration: 200,
+        useNativeDriver: false,
+        easing: Easing.sin,
+      }),
+      Animated.sequence([
+        Animated.delay(100),
+        Animated.timing(animatedWidth, {
+          toValue: 1,
+          duration: 120,
+          useNativeDriver: false,
+        }),
+      ]),
+    ]),
+    Animated.timing(animatedPosition, {
+      toValue: 40,
+      duration: 120,
+      useNativeDriver: false,
+    }),
+    Animated.delay(1300),
+    Animated.timing(animatedPosition, {
+      toValue: 44,
+      duration: 120,
+      useNativeDriver: false,
+    }),
+    Animated.parallel([
+      Animated.timing(animatedPosition, {
+        toValue: 0,
+        duration: 200,
+        useNativeDriver: false,
+        easing: Easing.sin,
+      }),
+      Animated.sequence([
+        Animated.delay(100),
+        Animated.timing(animatedWidth, {
+          toValue: 0,
+          duration: 120,
+          useNativeDriver: false,
+        }),
+      ]),
+    ]),
+  ]).start();
