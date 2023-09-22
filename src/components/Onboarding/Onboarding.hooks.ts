005e2600fc4f6f665f0acae924b08753df674395
Yury <ymorpheus@gmail.com>
2023-09-23 00:32:55 +0200

    fix: prevent scrolling on last pages (#99)
    Co-authored-by: Yury Kurouski <yury_kurouski@epam.com>

--- src/components/Onboarding/Onboarding.hooks.ts
+++ src/components/Onboarding/Onboarding.hooks.ts
@@ -24,25 +24,29 @@ export const useOnboardingHandlers: UseOnboardingHandlers = flatListRef => {
     const absoluteDiff = Math.abs(scrollStart) - Math.abs(contentOffset.x);
 
     if ((roundedEnd === 3 || roundedEnd === 0) && absoluteDiff === 0) return;
+    if (velocity.x > 0 && currentPage === 2) return;
+    if (velocity.x < 0 && currentPage === 0) return;
 
-    if (Math.abs(velocity.x) > 0.5) {
-      return flatListRef?.current?.scrollToIndex({
-        animated: true,
-        index: absoluteDiff < 0 ? roundedEnd + 1 : roundedEnd - 1,
-      });
-    }
+    try {
+      if (Math.abs(velocity.x) > 0.5) {
+        return flatListRef?.current?.scrollToIndex({
+          animated: true,
+          index: absoluteDiff < 0 ? roundedEnd + 1 : roundedEnd - 1,
+        });
+      }
 
-    if (Math.abs(absoluteDiff) > SCREEN_WIDTH / 2) {
-      flatListRef?.current?.scrollToIndex({
-        animated: true,
-        index: roundedEnd,
-      });
-    } else {
-      flatListRef?.current?.scrollToIndex({
-        animated: true,
-        index: currentPage,
-      });
-    }
+      if (Math.abs(absoluteDiff) > SCREEN_WIDTH / 2) {
+        flatListRef?.current?.scrollToIndex({
+          animated: true,
+          index: roundedEnd,
+        });
+      } else {
+        flatListRef?.current?.scrollToIndex({
+          animated: true,
+          index: currentPage,
+        });
+      }
+    } catch (e) {}
   };
 
   const handleStartDrag: EventHandler = event => {
