115aedfe94340971f3bb5d272dd21d649f9a9d8a
Yury Kurouski <yury_kurouski@epam.com>
2023-09-22 22:04:26 +0200

    chore: add configs

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
  */
 
 import { AppRegistry } from 'react-native';
+import { setupDefaultFlipperReporter } from 'react-native-performance-flipper-reporter';
+import { initAsyncStorageInspector } from 'asyncstorage-inspector-flipper';
 
 import App from './src/App';
 import { name as appName } from './app.json';
+
+if (__DEV__) {
+  setupDefaultFlipperReporter();
+  initAsyncStorageInspector();
+}
 
 AppRegistry.registerComponent(appName, () => App);
