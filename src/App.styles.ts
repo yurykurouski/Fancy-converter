6c59dae79c71723bb4d2f8f0abacb29bf1cd8136
Yury Kurouski <yury_kurouski@epam.com>
2023-09-27 01:23:33 +0200

    fix: refresh layout styles

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -3,9 +3,8 @@ import { useTheme } from 'hooks';
 export const useStyles = () =>
   useTheme(theme => {
     return {
-      backgroundStyle: {
-        height: '100%',
-        width: '100%',
+      container: {
+        flex: 1,
         backgroundColor: theme.APP_BACKGROUND_PRIMARY,
       },
     };
--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -3,13 +3,16 @@ import { isAndroid } from 'utils/platform';
 
 export const useStyles = () =>
   useTheme(theme => ({
+    container: {
+      paddingTop: 25,
+      alignSelf: 'center',
+    },
     header: {
       fontSize: 18,
       fontWeight: 'bold',
       ...(isAndroid && { fontFamily: 'monospace' }),
       textTransform: 'uppercase',
-      paddingTop: 25,
-      alignSelf: 'center',
+
       color: theme.FONT_PRIMARY_COLOR,
     },
   }));
