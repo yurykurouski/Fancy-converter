825c41019e77b59256779876a1be9a22976bc246
Yury Kurouski <yury_kurouski@epam.com>
2023-09-29 16:10:58 +0200

    fix config

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,17 +23,7 @@ module.exports = {
     'simple-import-sort/exports': 'error',
     'simple-import-sort/imports': 'error',
     'react/display-name': 'off',
-    'no-unused-vars': 'off',
     'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
-    ],
     'no-console': 'warn',
   },
   settings: {
