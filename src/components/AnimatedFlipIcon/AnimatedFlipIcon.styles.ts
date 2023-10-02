059d6e9c9e29ef51715cc7442f27bbf06db756fe
Yury <ymorpheus@gmail.com>
2023-10-02 11:57:05 +0200

    fix: initial styles (#131)
    Co-authored-by: Yury Kurouski <yury_kurouski@epam.com>

--- src/components/AnimatedFlipIcon/AnimatedFlipIcon.styles.ts
+++ src/components/AnimatedFlipIcon/AnimatedFlipIcon.styles.ts
@@ -3,5 +3,10 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   iconContainer: {
     position: 'absolute',
+    transform: [
+      {
+        rotateY: '180deg',
+      },
+    ],
   },
 });
