5c7c2f7dd0fc5f6c1a0ac348aa2296b182cc9588
Yury Kurouski <yury_kurouski@epam.com>
2023-10-18 11:52:12 +0200

    fix: naming

--- src/resources/avaliable-currencies.ts
+++ src/resources/avaliable-currencies.ts
@@ -1,9 +1,9 @@
 import { AvailableCryptoNames, AvailableFiatNames, ECurrencyType } from 'types';
 
-const AvailableFiatNames = Object.values(AvailableFiatNames).sort();
+const availableFiatNames = Object.values(AvailableFiatNames).sort();
 const availableCryptoNames = Object.values(AvailableCryptoNames).sort();
 
 export default {
-  [ECurrencyType.FIAT]: AvailableFiatNames,
+  [ECurrencyType.FIAT]: availableFiatNames,
   [ECurrencyType.CRYPTO]: availableCryptoNames,
 };
