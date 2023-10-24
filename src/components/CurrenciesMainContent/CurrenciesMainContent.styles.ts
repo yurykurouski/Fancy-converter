99105dfd69a7d73d4747b5f9715309bf62d46346
Yury Kurouski <yury_kurouski@epam.com>
2023-10-24 22:26:31 +0200

    fix: add padding to keep content inside the container

--- src/components/CurrenciesMainContent/CurrenciesMainContent.styles.ts
+++ src/components/CurrenciesMainContent/CurrenciesMainContent.styles.ts
@@ -3,7 +3,10 @@ import { isIos } from 'utils';
 
 export const useStyles = () =>
   useTheme((_, { bottom }) => ({
-    container: { flex: 1, ...(isIos && { paddingBottom: bottom + 68 }) },
+    container: {
+      flex: 1,
+      ...(isIos && { paddingBottom: bottom + 68 }),
+    },
     gestureHandler: {
       minHeight: 70,
       flexGrow: 100,
