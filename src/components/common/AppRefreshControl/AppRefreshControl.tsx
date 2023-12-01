85f7bfd80ef0dc590ee1c0a5e641ff19d11b42e1
Yury Kurouski <yury_kurouski@epam.com>
2023-12-01 21:21:49 +0100

    feat: add AppRefreshControl component

---
+++ src/components/common/AppRefreshControl/AppRefreshControl.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  RefreshControl as RNRefreshControl,
+  RefreshControlProps,
+} from 'react-native';
+import { useSelector } from 'react-redux';
+import { THEME_COLORS } from 'assets/colors';
+import { selectColorSchemeState } from 'store/ui/selectors';
+
+export const AppRefreshControl = ({
+  refreshing,
+  onRefresh,
+  ...rest
+}: RefreshControlProps) => {
+  const { colorScheme } = useSelector(selectColorSchemeState);
+
+  return (
+    <RNRefreshControl
+      refreshing={refreshing}
+      onRefresh={onRefresh}
+      // android
+      colors={[THEME_COLORS[colorScheme!].FONT_PRIMARY_COLOR]}
+      progressBackgroundColor={
+        THEME_COLORS[colorScheme!].APP_BACKGROUND_PRIMARY
+      }
+      //ios
+      tintColor={THEME_COLORS[colorScheme!].FONT_PRIMARY_COLOR}
+      {...rest}
+    />
+  );
+};
