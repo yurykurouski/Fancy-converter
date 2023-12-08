a81d5d8be0326aaf4c13c2d99ec397676ef06198
Yury Kurouski <yury_kurouski@epam.com>
2023-12-08 18:41:22 +0100

    feat: add mail icon

---
+++ src/assets/icons/MailIcon.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
+import { Path, Svg } from 'react-native-svg';
+import { useSelector } from 'react-redux';
+import { ColorsDark, ColorsLight, THEME_COLORS } from 'assets/colors';
+import { selectColorSchemeState } from 'store/colorScheme/selectors';
+
+type TProps = {
+  size: number;
+  style?: StyleProp<ViewStyle>;
+  color?: ColorsLight | ColorsDark;
+};
+
+export const MailIcon = ({ size, style, color }: TProps) => {
+  const { colorScheme } = useSelector(selectColorSchemeState);
+
+  return (
+    <Svg width={size} height={size} viewBox="0 -960 960 960" style={style}>
+      <Path
+        d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"
+        fill={color ?? THEME_COLORS[colorScheme!].FONT_PRIMARY_COLOR}
+      />
+    </Svg>
+  );
+};
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,5 @@
 export { BookmarkIcon } from './BookmarkIcon';
 export { CheckIcon } from './CheckIcon';
 export { DeleteSweepIcon } from './DeleteSweepIcon';
+export { MailIcon } from './MailIcon';
 export { MenuIcon } from './MenuIcon';
