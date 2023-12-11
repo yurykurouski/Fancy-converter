61f56d8c7c635b471746832dba3a545ee70dbc54
Yury <ymorpheus@gmail.com>
2023-12-11 15:03:56 +0100

    fix: remove blur (#226)
    Co-authored-by: Yury Kurouski <yury_kurouski@epam.com>

--- src/components/ControlsMenu/ControlsMenu.styles.ts
+++ src/components/ControlsMenu/ControlsMenu.styles.ts
@@ -1,15 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { useTheme } from 'hooks';
 
-export const styles = StyleSheet.create({
-  controlsContainer: {
-    position: 'absolute',
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingLeft: 18,
-    paddingRight: 10,
-    paddingVertical: 10,
-    bottom: 0,
-  },
-});
+export const useStyles = () =>
+  useTheme(theme => ({
+    container: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      bottom: 0,
+      flexDirection: 'row',
+      backgroundColor: theme.ACCENT_COLOR_DARKER,
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      paddingLeft: 18,
+      paddingRight: 10,
+      paddingVertical: 10,
+      borderTopLeftRadius: 20,
+      borderTopRightRadius: 20,
+      elevation: 10,
+    },
+  }));
