0d8007dc846b57699432835e19f2ce6c78390bbc
Yury Kurouski <yury_kurouski@epam.com>
2023-12-15 17:03:45 +0100

    refactor: change back button style

--- src/components/common/ScreenHeaderLeft/ScreenHeaderLeft.tsx
+++ src/components/common/ScreenHeaderLeft/ScreenHeaderLeft.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Pressable, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RightArrowIcon } from 'assets/icons';
+import { ButtonWithIPadOSInteraction } from 'components/common/ButtonWithIPadOSInteraction';
 import { ICON_BUTTON_SIZE } from 'constants/index';
 
 import { useStyles } from './ScreenHeaderLeft.styles';
@@ -11,10 +11,10 @@ export const ScreenHeaderLeft = () => {
   const { goBack } = useNavigation();
 
   return (
-    <Pressable onPress={goBack} style={styles.container}>
-      <View style={styles.iconContainer}>
-        <RightArrowIcon size={ICON_BUTTON_SIZE} />
-      </View>
-    </Pressable>
+    <ButtonWithIPadOSInteraction
+      onPress={goBack}
+      containerStyle={styles.iconContainer}>
+      <RightArrowIcon size={ICON_BUTTON_SIZE} />
+    </ButtonWithIPadOSInteraction>
   );
 };
