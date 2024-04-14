8d6b50e8cf6b6825e7dafd60c3bca6b108e0d0dc
Yury Kurouski <ymorpheus@gmail.com>
2024-04-14 21:58:01 +0200

    refactor: remove redux for colorScheme

--- src/hooks/store/UIStatus/index.ts
+++ src/hooks/store/UIStatus/index.ts
@@ -1,3 +1,1 @@
 export { useSetEditMode } from './useSetEditMode';
-export { useSwitchAppearanceBehavior } from './useSwitchAppearanceBehavior';
-export { useSwitchColorScheme } from './useSwitchColorScheme';
