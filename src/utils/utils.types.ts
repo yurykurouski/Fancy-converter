f97f8b0961c46bfa7f55aec4863fe2d671c34c35
Yury Kurouski <ymorpheus@gmail.com>
2024-04-25 00:16:39 +0200

    chore: remove dead code

--- src/utils/utils.types.ts
+++ src/utils/utils.types.ts
@@ -4,11 +4,6 @@ export type OnlyCourses = {
   [key in TAvailableCurrenciesNames]: number;
 };
 
-export type TGetCoursesForSelectedCurrencies = (
-  exchangeCourse: OnlyCourses,
-  selectedCurrencies: TAvailableCurrenciesNames[] | [],
-) => OnlyCourses | undefined;
-
 export type ShowNoConnectionAlert = (
   onPress?: (value?: any) => void,
   saveDate?: string | null,
