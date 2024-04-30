2d2fff0292b52538dfb6641d294564a2ad613695
Yury Kurouski <ymorpheus@gmail.com>
2024-04-30 19:19:44 +0200

    feat: animate bs handler

--- src/components/CurrenciesBottomSheet/hooks/useRenderHandler/useRenderHandler.tsx
+++ src/components/CurrenciesBottomSheet/hooks/useRenderHandler/useRenderHandler.tsx
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
+import Animated, {
+  cancelAnimation,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withSequence,
+  withSpring,
+} from 'react-native-reanimated';
 import { ButtonWithIPadOSInteraction } from 'components/common/ButtonWithIPadOSInteraction';
+import { selectedCurrenciesStore } from 'store/selectedCurrenciesStore';
+import { useSnapshot } from 'valtio';
 
 import { useStyles } from './styles';
 
 export const useRenderHandler = (onPress?: () => void) => {
   const styles = useStyles();
+  const { currencies } = useSnapshot(selectedCurrenciesStore);
+
+  const animatedHandlerValue = useSharedValue(1);
+
+  const isEmpty = !Object.keys(currencies).length;
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scaleX: animatedHandlerValue.value,
+        },
+      ],
+    };
+  });
+
+  useEffect(() => {
+    if (isEmpty) {
+      animatedHandlerValue.value = withRepeat(
+        withSequence(withDelay(3000, withSpring(1)), withSpring(1.2)),
+        -1,
+        true,
+      );
+    } else {
+      cancelAnimation(animatedHandlerValue);
+      animatedHandlerValue.value = withSpring(1);
+    }
+  }, [isEmpty, animatedHandlerValue]);
 
   return () => (
-    <View style={styles.handleContainer}>
+    <Animated.View style={[styles.handleContainer, animatedStyle]}>
       <ButtonWithIPadOSInteraction
         containerStyle={styles.handlePressable}
         onPress={onPress}
         hitSlop={10}>
         <View style={styles.handle} />
       </ButtonWithIPadOSInteraction>
-    </View>
+    </Animated.View>
   );
 };
