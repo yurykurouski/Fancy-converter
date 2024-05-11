cc9f8f92d090648634be1ddfaf14372d9479da87
Yury Kurouski <ymorpheus@gmail.com>
2024-05-11 23:23:39 +0200

    feat: colors config

---
+++ platform-colors.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  colors: {
+    APP_BACKGROUND_PRIMARY: {
+      dark: '#202124',
+      light: '#f0f0f3',
+    },
+    FONT_PRIMARY_COLOR: {
+      dark: '#ffffff',
+      light: '#363535',
+    },
+    FONT_PRIMARY_COLOR_INVERTED: {
+      dark: '#363535',
+      light: '#ffffff',
+    },
+    FONT_SECONDARY_COLOR: {
+      dark: '#ffffff',
+      light: '#fafcfe',
+    },
+    FONT_COLOR_FADED: {
+      dark: '#808080',
+      light: '#808080',
+    },
+    ELEMENT_FADE_OR_BACKGROUND: {
+      dark: '#323236',
+      light: '#E1E1E3',
+    },
+    ELEMENT_FADE_OR_BACKGROUND_DARKER: {
+      dark: '#21212b',
+      light: '#DADAE1',
+    },
+    ELEMENT_FADE_OR_BACKGROUND_LIGHTER: {
+      dark: '#424146',
+      light: '#D3D3D5',
+    },
+    ACCENT_COLOR_LIGHTER: {
+      dark: '#4F378B',
+      light: '#1a73e8',
+    },
+    ACCENT_COLOR_DARKER: {
+      dark: '#1a1921',
+      light: '#e9e9f0',
+    },
+    SUNSET_ORANGE: {
+      dark: '#ef5350',
+      light: '#ef5350',
+    },
+    OVERLAY: {
+      dark: '#20212499',
+      light: '#f0f0f399',
+    },
+  },
+  javascript: {
+    typescript: true,
+    outputDirectory: 'src/colors/',
+  },
+  ios: {
+    outputDirectory: 'ios/Converter/Colors.xcassets/',
+  },
+  android: {
+    outputDirectory: 'android/app/src/main/res/',
+  },
+};
